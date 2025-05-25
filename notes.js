5a4535148f85b5ff6f220404e15594c3a357e12a
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-05-25 21:19:02 +0530

    Add files via upload

--- notes.js
+++ notes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res)=>{
-   let obj = {
-        a: 'thios',
-        number :34
-    }
-    res.json(obj);
+const User = require('../models/User');
+// create  a user using: Post '/api/auth". dosent require Auth
+router.post('/', (req, res)=>{
+  console.log(req.body);
+  const user =User(req.body); 
+  user.save()
+  res.send("hello");
 })
-module.exports = router;
+module.exports = router;                    
