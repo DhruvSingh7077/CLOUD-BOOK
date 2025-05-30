51164ef6388a5558927fbaaf10c3f342b9da2dc8
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-05-30 21:02:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+// REMOVE THIS PART
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const UserSchema =  new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+});
+module.exports = mongoose.model('User', UserSchema);
