bdbf762bbf0af9081b3f1a1799ca04e4a77523d2
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-05-31 20:14:38 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -33,3 +33,26 @@ const UserSchema =  new mongoose.Schema({
     },
 });
 module.exports = mongoose.model('User', UserSchema);
+// REMOVE THJIS PART
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const UserSchema =  new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+});
+module.exports = mongoose.model('User', UserSchema);
