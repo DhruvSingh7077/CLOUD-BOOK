08a7dab1db0b9fda8b447d0e518a9e8ba70a62d3
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-01 22:51:53 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -6,15 +6,4 @@ router.get('/', (req, res)=>{
     res.json([])
 })
 module.exports = router
- const mongoose = require('mongoose');
 
-const connectToMongo = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/mydb');
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-  }
-};
-
-module.exports = connectToMongo;
--- notes.js
+++ notes.js
@@ -9,26 +9,4 @@ router.post('/', (req, res)=>{
   res.send("hello");
 })
 module.exports = router;    
-// you have to remove this part 
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-const UserSchema =  new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    },
-});
-module.exports = mongoose.model('User', UserSchema);                
+
