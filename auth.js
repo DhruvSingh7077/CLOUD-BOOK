53c1d262ecbf70b391e7907c95a39167135d61d6
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-02 22:21:15 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 router.get('/', (req, res)=>{
-   
-    res.json([])
+   console.log(req.body);
+   const user = User(req.body);
+   user.save()
+   res.send(req.body);
+    
+   router.post('/',[
+    body('name').islength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:5}),
+   ])
 })
 module.exports = router
 
