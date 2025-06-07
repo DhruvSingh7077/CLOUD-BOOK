542a1ce1ec344de6f1cab23e8804c27cec02c534
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-07 23:28:06 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,39 +1,30 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const {body, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save()
-   res.send(req.body);
+// create a USER using : POST "/api/auth/". Doesn't require auth
+router.post('/',[
+   body('name','enter a valid name').isLength({min :3}),
+   body('email','enter a valid email').isEmail(),
+   body('password','password must be atleat 5 character').isLength({min:5}),
+], (req, res)=>{
+   const errors = validationResult(req);
+   if (!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+   }
+   User.create({
+      name:req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+   }).then(user => res.json(user));
+   
+
     
-   router.post('/',[
-    body('name').islength({min:3}),
-    body('email').isEmail(),
-    body('password').isLength({min:5}),
-   ])
+  
 })
 module.exports = router
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router();
-const {body, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save()
-   res.send(req.body);
-    
-   router.post('/',[
-    body('name').islength({min:3}),
-    body('email').isEmail(),
-    body('password').isLength({min:5}),
-   ])
-})
-module.exports = router
 
 
 
