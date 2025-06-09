123cd32628eed53e3442dea698ac96a8bd83bd52
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-09 23:34:49 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -3,21 +3,35 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-// create a USER using : POST "/api/auth/". Doesn't require auth
-router.post('/',[
+// create a USER using : POST "/api/auth/createuser". no login required
+router.post('/createuser',[
    body('name','enter a valid name').isLength({min :3}),
    body('email','enter a valid email').isEmail(),
    body('password','password must be atleat 5 character').isLength({min:5}),
-], (req, res)=>{
-   const errors = validationResult(req);
+], async(req, res)=>{
+   // if there are eroors return bad request and the error
+      const errors = validationResult(req);
    if (!errors.isEmpty()){
       return res.status(400).json({errors:errors.array()});
    }
-   User.create({
-      name:req.body.name,
+   // check whether the user with email exists already
+try{
+
+   let user = await User.findOne({email: req.body.email});
+   if(user){
+      return res.status(400).json({error: "sorry a user with this email already exists"})
+   }
+   // creating a new user
+   user= await User.create({
+      name:req.body.name, 
       password: req.body.password,
       email: req.body.email,
-   }).then(user => res.json(user));
+   })
+   res.json (user)
+} catch(error) {
+   console.error(error.message);
+   res.status(500).send("some Error occured");
+}
    
 
     
