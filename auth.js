b6b3518efada21316c7e7f15ac5825704c34bdba
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-11 19:05:12 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -37,18 +37,38 @@ try{
       }
    }
     
-   const jwtData = jwt.sign(data, JWT_SECRET);
-   console.log(jwtData);
-   res.json (user)
+   const authtoken = jwt.sign(data, JWT_SECRET);
+   
+   //res.json (user)
+   res.json({authtoken})
 } catch(error) {
    console.error(error.message);
    res.status(500).send("some Error occured");
 }
    
-
-    
-  
 })
+// Authentication a User using: POST "/api/auth/login". no login required"
+router.post('/createuser',[
+   body('email','enter a valid email').isEmail(),
+   body('password','password cannot be blank').exists(),
+], async(req, res)=>{
+  
+   // if there are eroors return bad request and the error
+   const errors = validationResult(req);
+   if (!errors.isEmpty()){
+      return res.status(400).json({ errors:errors.array()});
+   }
+   const {email, password} = req.body;
+   try{
+  let user =User.findOne({email});
+  if(!user){
+   return res.status(400).json({error: "please try to login with correct credentials"});
+  }
+  const passwordCompare = bcrypt.compare(password, user.password);
+   } catch (error) {
+
+   }
+   })
 module.exports = router
 
 
