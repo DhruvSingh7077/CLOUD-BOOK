1683f3d8fae846e2e76568dea63ae030087f5184
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-12 21:19:14 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -86,7 +86,27 @@ router.post('/login',[
 
    })
 module.exports = router
+// remove this afterwards
+ 
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if(!passwordCompare){
+   return res.status(400).json({error: "please try to login with correct credentials"});
+  }
 
+  const data = {
+      user:{
+         id: user.id
+      }
+   } 
+ const authtoken = jwt.sign(data, JWT_SECRET);
+   
+   
+   res.json({authtoken})
+
+   }catch(error) {
+   console.error(error.message);
+   res.status(500).send("Internal Server Error");
+}
 
 
 
