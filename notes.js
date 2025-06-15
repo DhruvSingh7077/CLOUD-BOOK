ddf583d1622bb853ab48261ba49f13f12bcb02d5
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-15 22:28:07 +0530

    Add files via upload

--- notes.js
+++ notes.js
@@ -10,6 +10,24 @@ router.get('/fetchallnotes',fetchuser, (req, res)=>{
 module.exports = router;    
 
     
+// remove
+const express = require('express');
+const router = express.Router();
+//const User = require('../models/User');
+ var fetchuser = require('../middleware/fetchuser');  
+//  Route 1:Get All the Notes using: GET '/api/auth/getuser".  login required
+router.get('/fetchallnotes',fetchuser, (req, res)=>{
+ 
+  res.json("hello");
+})
+module.exports = router;    
+
+    
+
+   
+
+
+
 
    
 
