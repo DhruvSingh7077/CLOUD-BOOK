ab50fc2f8c226450c1d8ae2dbf495393636d8332
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-06-24 19:42:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
- const express = require('express');
-const connectToMongo = require('./db');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-connectToMongo(); 
-const app = express()
-const port = 5000 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.use(express.json())
-//available routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-// remove this 
- const express = require('express');
-const connectToMongo = require('./db');
-
-connectToMongo(); 
-const app = express()
-const port = 5000 
-
-app.use(express.json())
-//available routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
